a8a3c26b41931a3109c1ba208af130fb906253b9
Roni Palmon <roni.palmon@post.runi.ac.il>
2023-12-27 19:47:52 +0200

    new

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -9,30 +9,48 @@ public class OneOfEachStats1 {
 		int t = Integer.parseInt(args[0]);
 		boolean girl = false; 
 		boolean boy = false;
-		int sum = 0;
 		int sum_all= 0;
-		double averege = 0;
+		double averege = 0.0;
+		int twochildren = 0;
+		int threechildren = 0;
+		int fourchildren = 0;
+		int common = 0 ;
 		
 			for (int i = 0; i <= t; i++){
+				int sum = 0;
 				while(girl == false || boy == false){
 					double a = (Math.random() );
 					if (a >= 0.5){
 						girl = true;
-						System.out.print("g ");
 					}
 
 					else {
 						boy = true;
-						System.out.print("b ");
 					}
-					sum += 1;
-			}	
-			sum_all += sum;
-			averege = sum_all / t ; 
-			System.out.println("Averege: " + averege + " children to get at least one of each gender.");
+					sum ++ ;
+			}
+			if (sum == 2) {	
+				twochildren ++;
+			}
+			else if (sum == 3) {
+				threechildren ++;
+			}
+			else {
+				fourchildren ++;
+			}
+
+			sum_all += sum;	
 		}
 		
+		averege = sum_all / t ; 
+		common = Math.max(Math.max(twochildren,threechildren),fourchildren);
+		System.out.println("Averege: " + averege + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: "+ twochildren);
+		System.out.println("Number of families with 3 children: "+ threechildren);
+		System.out.println("Number of families with 4 or more children: "+ fourchildren);
+		System.out.println("The most common number of children is "+ common);
 	}
-	}
+}
+
 
 
